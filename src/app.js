7ce74e587ac913dcb0d8d63862a12ef2355ac63a
msd1001 <madhureshsingh1001@gmail.com>
2026-02-23 21:08:48 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -37,7 +37,86 @@ app.post("/signup", async (req, res) => {
     await user.save();
     res.send(" User Added Successfully");
   } catch (err) {
-    res.status(400).send("Bad Request", err.message);
+    res.status(400).send(err.message);
+  }
+});
+
+// GET users by Email id;
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+
+// GET oldest users by Email id if there are more than one user by same email id
+
+/*
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (user.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+*/
+
+// Feed API - Get/feed  get all users from database
+app.get("/feed", async (req, res) => {
+  // From which modal we want to Get data ===> user Modal, we want to get user data.
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body._id;
+  console.log(userId);
+
+  try {
+    const user = await User.findOneAndDelete({ _id: userId });
+    // const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted Successfully");
+  } catch (err) {
+    res.send(400).send("Something Went Wrong");
+  }
+});
+
+// Update data of the user
+
+app.patch("/user", async (req, res) => {
+  // req.body is coming from postman
+  // console.log(req.body);
+  const userId = req.body._id;
+  const data = req.body;
+  // if in the data we are sending some field which is not defined in the userSchema then that will not be updated
+
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("Data updated Successfully");
+  } catch (err) {
+    res.status(400).send(err.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,66 @@
 const mongoose = require("mongoose");
 // Below we have created user Schema
 // After connecting database and server in app file , define schema. STEP 0(B) then step1 will start from creating API
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    // mongoose will not allow insertion of record in database if required field value is not entered while doing registeration
+    // required check is added
+    // unique check
+    // All these check we are putting at database level
+    // All these are schema vaidation
+
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    // We cannot create a new user with already existing email-ID
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      // Custom validatons will work by default on new document insert , to enable it to work for update also we need to add runValidators : true in put and patch method.
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2E1ISMWr2Xdz5yB3pjWAgs0drcWCw9vya5A&s",
+    },
+    // if user did  not pass about field then it will be automatically created with default value in db because of default value check
+    about: {
+      type: String,
+      default: "This is default about user",
+    },
+
+    // if  type is an array and value is not provided then it will automatically take empty value in array.
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true },
+);
 
 // Now we will be creating Mongoose model
 // Below we have created user Model
