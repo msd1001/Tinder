1915a8ba334bdd2e900ed08a65c3311630823e95
msd1001 <madhureshsingh1001@gmail.com>
2026-04-04 13:49:50 +0530

    save user

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -51,8 +51,13 @@ authRouter.post("/signup", async (req, res) => {
 
     // database operation must be performed in try & catch block
     // with help of below code we r able to save data to database
-    await user.save();
-    res.send(" User Added Successfully");
+    const savedUser = await user.save();
+    const token = await savedUser.getJWT();
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
+
+    res.json({ message: " User Added Successfully", data: savedUser });
   } catch (err) {
     res.status(400).send(err.message);
   }
