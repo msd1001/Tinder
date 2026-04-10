ba1bf4c47e2863341e97b0103082eb33032aa8ef
msd1001 <madhureshsingh1001@gmail.com>
2026-04-10 22:44:49 +0530

    added env file

--- src/config/database.js
+++ src/config/database.js
@@ -4,9 +4,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   // this string we will get when we create cluster on mongodb database website, cluster could have many database each database have document
-  await mongoose.connect(
-    "mongodb+srv://madhureshsingh1001_db_user:iW8EsiXA37e7GGmm@clusternamasteharsh.0tsi7s9.mongodb.net/devTinder",
-  );
+  console.log(process.env.DB_CONNECTION_SECRET);
+  await mongoose.connect(process.env.DB_CONNECTION_SECRET);
 
   // database name is Tinder.
   // inside Tinder we have collection called users
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -42,7 +42,7 @@ const userAuth = async (req, res, next) => {
     }
 
     // validate the token
-    const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
+    const decodedObj = await jwt.verify(token, process.env.JWT_SECRET);
 
     const { _id } = decodedObj;
 
