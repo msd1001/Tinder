98d2f19b45c2ecc49afe6bae58063381464499fd
msd1001 <madhureshsingh1001@gmail.com>
2026-04-10 22:55:35 +0530

    key value

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -1,6 +1,7 @@
 const { SESClient } = require("@aws-sdk/client-ses");
 // Set the AWS Region.
 const REGION = "eu-north-1";
+//
 // Credentials are automatically resolved using the AWS SDK credential provider chain.
 // For more information, see https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/setting-credentials-node.html
 // Create SES service object.
